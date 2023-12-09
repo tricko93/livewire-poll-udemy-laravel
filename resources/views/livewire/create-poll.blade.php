ebf8ad2181c170efdc6e782d0fa2fd22b9dbb823
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-12-10 00:29:33 +0100

    feature Add validation rules and error messages

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -4,6 +4,10 @@
 
         <input type="text" wire:model.live="title" />
 
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
         </div>
@@ -17,6 +21,9 @@
                         <input type="text" wire:model="options.{{ $index }}" />
                         <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
